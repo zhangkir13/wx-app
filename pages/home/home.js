9d1d2f8b08b9e1a8651673ccaabf18861b9b69ac
zhangkir13 <zhangkir13@users.noreply.github.com>
2024-03-18 23:25:18 +0000

    备份

---
+++ pages/home/home.js
@@ -0,0 +1,38 @@
+var base64 = require("../../image/base64.js");
+
+Page({
+  data: {
+    movies: [
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
+      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+    ] 
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  onLoad: function () {
+    this.setData({
+      icon20: base64.icon20,
+      icon60: base64.icon60
+    });
+  }
+})
